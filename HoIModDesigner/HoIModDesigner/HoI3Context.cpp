866f3384dc4383d3ba76fb9d089c4b0a198b5a0e
raubwuerger77 <hannuschka@web.de>
2014-06-18 23:12:20 +0200

    Nix

--- HoIModDesigner/HoIModDesigner/HoI3Context.cpp
+++ HoIModDesigner/HoIModDesigner/HoI3Context.cpp
@@ -1,7 +1,8 @@
 #include "stdafx.h"
 #include "HoI3Context.h"
 
-HoI3Context::HoI3Context() : m_BasePath("E:\\Spiele\\HoI3"),
+HoI3Context::HoI3Context() 
+	: m_BasePath("D:\\Spiele\\HoI3"),
 	m_PathTFH("tfh"),
 	m_ProvincesBMP("map\\provinces.bmp"),
 	m_DefinitionCSV("map\\definition.csv"),
