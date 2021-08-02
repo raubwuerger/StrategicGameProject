b96244a02e3514a3ed59fd846d2254b3c0d55e8c
raubwuerger <hannuschka@web.de>
2021-08-02 18:01:15 +0200

    Initial commit.

---
+++ GameDemonstrator/GameSetting.h
@@ -0,0 +1,10 @@
+#pragma once
+
+class GameSetting
+{
+public:
+	static QString		CurrentSaveGameName;
+	static QString		CurrentTurn;
+	static QString		DifficultyLevel;
+	static QString		PlayerCount;
+};
