e51932b4f999705eeb21f4c529a430688275e0ca
raubwuerger <hannuschka@web.de>
2021-08-14 17:23:54 +0200

    Further development.

--- GameDemonstrator/controller/KeyEventController.cpp
+++ GameDemonstrator/controller/KeyEventController.cpp
@@ -1,11 +1,31 @@
 #include "stdafx.h"
 #include "KeyEventController.h"
 #include "map\MapUnitItem.h"
+#include "map\MapHexItemRepository.h"
+#include "map\MapHexItem.h"
+
+QMap<int, QSize> KeyEventController::MovementTypeColumEven;
+QMap<int, QSize> KeyEventController::MovementTypeColumOdd;
+
+KeyEventController::KeyEventController()
+{
+	CreateMovementTypes();
+}
 
 void KeyEventController::HandleKeyPressEvent(MapUnitItem* mapUnitItem, QKeyEvent* keyEvent)
 {
+	int mapHexItemId = mapUnitItem->GetMapHexItemId();
+
+	MapHexItem* mapHexItem = MapHexItemRepository::GetInstance()->GetMapHexItemById(mapHexItemId);
+	const QPointF& mapHexItemCenterPoint = mapHexItem->GetCenterPoint();
+
 	if (Qt::Key_1 == keyEvent->key())
 	{
+		if (false == IsMovementDirectionValid(1, mapHexItem))
+		{
+			return;
+		}
+
 		mapUnitItem->moveBy(-72, 42);
 		return;
 	}
@@ -34,6 +54,49 @@ void KeyEventController::HandleKeyPressEvent(MapUnitItem* mapUnitItem, QKeyEvent
 		mapUnitItem->moveBy(72, -42);
 		return;
 	}
+
+	if (Qt::Key_Escape == keyEvent->key())
+	{
+		mapUnitItem->setSelected(false);
+		return;
+	}
+}
+
+bool KeyEventController::IsMovementDirectionValid(int movementDirection, const MapHexItem* mapHexItem) const
+{
+	bool isEven = mapHexItem->GetCol() % 2;
+	const QSize source(mapHexItem->GetRow(), mapHexItem->GetCol());
+	if (true == isEven)
+	{
+		if (false == MovementTypeColumEven.contains(movementDirection))
+		{
+			return false;
+		}
+		QSize movementDirectionOffset = MovementTypeColumEven[movementDirection];
+		QSize destination = source + movementDirectionOffset;
+		MapHexItem* destinationMapHexItem = MapHexItemRepository::GetInstance()->GetMapHexItemByRowCol(destination.width(), destination.height());
+		if (nullptr == destinationMapHexItem)
+		{
+			return false;
+		}
+		return true;
+	}
+	if (false == isEven)
+	{
+		if (false == MovementTypeColumOdd.contains(movementDirection))
+		{
+			return false;
+		}
+		QSize movementDirectionOffset = MovementTypeColumOdd[movementDirection];
+		QSize destination = source + movementDirectionOffset;
+		MapHexItem* destinationMapHexItem = MapHexItemRepository::GetInstance()->GetMapHexItemByRowCol(destination.width(), destination.height());
+		if (nullptr == destinationMapHexItem)
+		{
+			return false;
+		}
+		return true;
+	}
+
 }
 
 void KeyEventController::HandleKeyReleaseEvent(MapUnitItem* mapUnitItem, QKeyEvent* keyEvent)
@@ -62,4 +125,27 @@ void KeyEventController::HandleKeyReleaseEvent(MapUnitItem* mapUnitItem, QKeyEve
 	{
 		return;
 	}
+}
+
+void KeyEventController::CreateValidMovementPositions(MapHexItem* mapHexItem)
+{
+	int col = mapHexItem->GetCol();
+	int row = mapHexItem->GetRow();
+}
+
+void KeyEventController::CreateMovementTypes()
+{
+	MovementTypeColumEven.insert(7, QSize(-1, -1));
+	MovementTypeColumEven.insert(8, QSize(-1, 0));
+	MovementTypeColumEven.insert(9, QSize(-1, 1));
+	MovementTypeColumEven.insert(1, QSize(0, -1));
+	MovementTypeColumEven.insert(2, QSize(+1, 0));
+	MovementTypeColumEven.insert(9, QSize(0, +1));
+
+	MovementTypeColumOdd.insert(7, QSize(0, -1));
+	MovementTypeColumOdd.insert(8, QSize(-1, 0));
+	MovementTypeColumOdd.insert(9, QSize(0, 1));
+	MovementTypeColumOdd.insert(1, QSize(+1, -1));
+	MovementTypeColumOdd.insert(2, QSize(+1, 0));
+	MovementTypeColumOdd.insert(3, QSize(+1, +1));
 }
--- GameDemonstrator/controller/KeyEventController.h
+++ GameDemonstrator/controller/KeyEventController.h
@@ -2,14 +2,27 @@
 #define KEYEVENTCONTROLLER_H
 
 class MapUnitItem;
+class MapHexItem;
 
 class KeyEventController
 {
 public:
 	/** */
+	KeyEventController();
+	/** */
 	void HandleKeyPressEvent(MapUnitItem* mapUnitItem, QKeyEvent* keyEvent);
 	/** */
 	void HandleKeyReleaseEvent(MapUnitItem* mapUnitItem, QKeyEvent* keyEvent);
+private:
+	/** */
+	void CreateValidMovementPositions(MapHexItem* mapHexItem);
+	/** */
+	static void CreateMovementTypes();
+	/** */
+	bool IsMovementDirectionValid(int movementDirection, const MapHexItem* mapHexItem) const;
+private:
+	static QMap<int, QSize> MovementTypeColumEven;
+	static QMap<int, QSize> MovementTypeColumOdd;
 };
 
 #endif // KEYEVENTCONTROLLER_H
