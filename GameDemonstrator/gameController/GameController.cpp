08fa0bca3707605e9617cad2eae244ec10700e8b
raubwuerger <hannuschka@web.de>
2021-08-30 22:08:43 +0200

    Initial commit.

---
+++ GameDemonstrator/gameController/GameController.cpp
@@ -0,0 +1,7 @@
+#include "stdafx.h"
+#include "GameController.h"
+
+GameController::GameController()
+{
+
+}
---
+++ GameDemonstrator/gameController/GameController.h
@@ -0,0 +1,12 @@
+#ifndef GAMECONTROLLER_H
+#define GAMECONTROLLER_H
+
+/** */
+class GameController
+{
+public:
+	/** */
+	GameController();
+};
+
+#endif // GAMECONTROLLER_H
