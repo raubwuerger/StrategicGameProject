2101fd14543269436260f95f3cf833782aef5333
raubwuerger <hannuschka@web.de>
2021-08-31 23:23:06 +0200

    Updated.

--- GameDemonstrator/gameController/GameConfig.cpp
+++ GameDemonstrator/gameController/GameConfig.cpp
@@ -1,9 +1,12 @@
 #include "stdafx.h"
 #include "GameConfig.h"
 
-QString		GameConfig::CurrentSaveGameName = ".\\savegames\\PlayerName_1900_01.xml";
-QString		GameConfig::CurrentTurn = "";
-QString		GameConfig::DifficultyLevel = "1";
-QString		GameConfig::PlayerCount = "2";
-QString		GameConfig::MapType = "1";
+QString			GameConfig::CurrentSaveGameName = ".\\savegames\\PlayerName_1900_01.xml";
+QString			GameConfig::CurrentTurn = "";
+QString			GameConfig::DifficultyLevel = "1";
+QString			GameConfig::PlayerCount = "2";
+QString			GameConfig::MapType = "1";
+QString			GameConfig::PlayerId = "2";
+ModelOwnerType*	GameConfig::OwnerType = nullptr;
+
 
--- GameDemonstrator/gameController/GameConfig.h
+++ GameDemonstrator/gameController/GameConfig.h
@@ -1,14 +1,18 @@
 #ifndef GAMECONFIG_H
 #define GAMECONFIG_H
 
+class ModelOwnerType;
+
 class GameConfig
 {
 public:
-	static QString		CurrentSaveGameName;
-	static QString		CurrentTurn;
-	static QString		DifficultyLevel;
-	static QString		PlayerCount;
-	static QString		MapType;
+	static QString			CurrentSaveGameName;
+	static QString			CurrentTurn;
+	static QString			DifficultyLevel;
+	static QString			PlayerCount;
+	static QString			MapType;
+	static QString			PlayerId;
+	static ModelOwnerType*	OwnerType;
 };
 
 #endif // GAMECONFIG_H
