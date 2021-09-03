44fcf59d660d7e55ed8fcc445b57661b93cee334
raubwuerger <hannuschka@web.de>
2021-09-03 12:24:05 +0200

    Initial commit.

---
+++ GameDemonstrator/GameDemonstrator_TEST/DomElementFinder_BOOLEAN_TEST.cpp
@@ -0,0 +1,18 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "DomElementFinder.h"
+#include <QString>
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace GameDemonstrator_TEST
+{
+	TEST_CLASS(DomElementFinder_BOOLEAN_TEST)
+	{
+	public:
+		TEST_METHOD(TestMethod1)
+		{
+		}
+
+	};
+}
