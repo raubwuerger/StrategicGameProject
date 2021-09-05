9228c0f64b6012783a7aae1d3d22a84d61c827d9
raubwuerger <hannuschka@web.de>
2021-09-05 13:39:39 +0200

    Polished GUI.

--- GameDemonstrator/dialogs/GameTurnDialog.cpp
+++ GameDemonstrator/dialogs/GameTurnDialog.cpp
@@ -1,6 +1,7 @@
 #include "stdafx.h"
 #include "GameTurnDialog.h"
 #include "gameController/GameConfig.h"
+#include "game/GameOwnerItem.h"
 
 GameTurnDialog::GameTurnDialog(QWidget *parent)
 	: QWidget(parent)
@@ -15,5 +16,23 @@ GameTurnDialog::~GameTurnDialog()
 
 void GameTurnDialog::SlotUpdateGameTurnInfo()
 {
-	ui.turnInfoEdit->setPlainText(QString::number(GameConfig::CurrentTurn));
+	ui.lineEditTurn->setText(QString::number(GameConfig::CurrentTurn));
+	
+	QString ownerName("");
+	QColor ownerColor(Qt::white);
+	GameOwnerItem* gameOwnerItem = GameConfig::Player;
+	if (nullptr != gameOwnerItem)
+	{
+		ownerName = gameOwnerItem->GetName();
+		ownerColor = gameOwnerItem->GetColor();
+	}
+	ui.lineEditOwner->setText(ownerName);
+
+	QPalette palette;
+	palette.setColor(QPalette::Base, ownerColor);
+	if (ownerColor == Qt::blue || ownerColor == Qt::green || ownerColor == Qt::darkGreen)
+	{
+		palette.setColor(QPalette::Text, Qt::white);
+	}
+	ui.lineEditOwner->setPalette(palette);
 }
