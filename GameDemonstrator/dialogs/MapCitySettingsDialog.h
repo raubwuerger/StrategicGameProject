8beaa05a1119786b5796511c7dfe3911549b3783
raubwuerger <hannuschka@web.de>
2021-09-06 20:31:33 +0200

    Introduced MapCitySettingsDialog.

---
+++ GameDemonstrator/dialogs/MapCitySettingsDialog.h
@@ -0,0 +1,13 @@
+#ifndef MAPCITYSETTINGSDIALOG_H
+#define MAPCITYSETTINGSDIALOG_H
+
+#include <QWidget>
+#include "ui_MapCitySettingsDialog.h"
+
+
+class MapCitySettingsDialog : public QWidget
+{
+	Q_OBJECT
+};
+
+#endif // MAPCITYSETTINGSDIALOG_H
