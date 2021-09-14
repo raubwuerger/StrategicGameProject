e1982c845bb14c2c184dacfb5db18e3f60937476
raubwuerger <hannuschka@web.de>
2021-09-14 12:25:00 +0200

    Corrected interface.

--- GameDemonstrator/helper/HeightMapPercentageSplitter.cpp
+++ GameDemonstrator/helper/HeightMapPercentageSplitter.cpp
@@ -1,5 +1,11 @@
 #include "stdafx.h"
 #include "HeightMapPercentageSplitter.h"
+#include <map>
+
+HeightMapPercentageSplitter::HeightMapPercentageSplitter()
+{
+
+}
 
 void HeightMapPercentageSplitter::ClearPercentageValue()
 {
@@ -16,22 +22,28 @@ bool HeightMapPercentageSplitter::RegisterPercentageValue(int percentage)
 	{
 		return false;
 	}
+	if ( std::find(PercentageValues.begin(), PercentageValues.end(), percentage) != PercentageValues.end() )
+	{
+		return false;
+	}
 	PercentageValues.push_back(percentage);
+	return true;
 }
 
-std::map<int, std::vector<int> > HeightMapPercentageSplitter::CalculatePercentageValue(std::vector<int> values)
+std::map<int, std::vector<float> > HeightMapPercentageSplitter::CalculatePercentageValue(std::vector<float> values)
 {
 	std::sort(values.begin(), values.end(), std::less<float>());
 	std::sort(PercentageValues.begin(), PercentageValues.end(), std::less<int>());
 
 	int valueCount = values.size();
-	std::map<int, std::vector<int> > splitterMap;
 
+	std::map<int, std::vector<float> > splitterMap;
 	int lastPercentageValue = 0;
 	std::vector<int>::iterator percentageIterator = PercentageValues.begin();
 	while (percentageIterator != PercentageValues.end())
 	{
-//		splitterMap.insert(values.[lastPercentageValue], values[percentageIterator]);
+		std::vector<float> sub(&values[lastPercentageValue], &values[*percentageIterator]);
+		splitterMap.insert(std::make_pair(*percentageIterator, sub));
 	}
 	return splitterMap;
 }
--- GameDemonstrator/helper/HeightMapPercentageSplitter.h
+++ GameDemonstrator/helper/HeightMapPercentageSplitter.h
@@ -2,18 +2,19 @@
 #define HEIGHTMAPCOLORMAPPER_H
 
 #include <vector>
+#include <map>
 
 class HeightMapPercentageSplitter
 {
 public:
 	/** */
-	HeightMapPercentageSplitter() {}
+	HeightMapPercentageSplitter();
 	/** */
 	void ClearPercentageValue();
 	/** Values between 1 and 100 are allowed */
 	bool RegisterPercentageValue(int percentage);
 	/** */
-	std::map<int, std::vector<int> > CalculatePercentageValue(std::vector<int> values);
+	std::map<int, std::vector<float> > CalculatePercentageValue(std::vector<float> values);
 private:
 	std::vector<int> PercentageValues;
 };
