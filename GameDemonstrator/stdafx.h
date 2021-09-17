3d775ab4944a864b7489e2ee910a57802134c76e
raubwuerger <hannuschka@web.de>
2021-09-17 19:31:46 +0200

    All map hex tiles now fit into MapView.

--- GameDemonstrator/stdafx.h
+++ GameDemonstrator/stdafx.h
@@ -1,9 +1,11 @@
 #include <QtWidgets>
 
-static int		NOT_INITIALIZED_INT		= -1;
-static QString	NOT_INITIALIZED_STRING	= "NOT_INITIALIZED";
-static bool		NOT_INITIALIZED_BOOL	= false;
-static double	NOT_INITIALIZED_DOUBLE	= -1.0;
+static const int		NOT_INITIALIZED_INT = -1;
+static const QString	NOT_INITIALIZED_STRING = "NOT_INITIALIZED";
+static const bool		NOT_INITIALIZED_BOOL = false;
+static const double	NOT_INITIALIZED_DOUBLE = -1.0;
 
-static int		GAME_UNIT_ID_EFFICIENCY = 9999;
-static QString	GAME_UNIT_STRING_EFFICIENCY = "Efficiency";
+static const int		GAME_UNIT_ID_EFFICIENCY = 9999;
+static const QString	GAME_UNIT_STRING_EFFICIENCY = "Efficiency";
+
+static const double	GLOBAL_HEXAGON_OFFSET = 1.0;
