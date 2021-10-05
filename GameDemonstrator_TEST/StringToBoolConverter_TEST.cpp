e98af5d29f76be88061d5683f0707d5267fc1d89
hannuschka@web.de <NdMTv8wtjmJ64AD>
2021-10-05 14:48:26 +0200

    Did some further work. Still not working.

---
+++ GameDemonstrator_TEST/StringToBoolConverter_TEST.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+#include "StringToBoolConverter.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace GameDemonstrator_TEST
+{
+	TEST_CLASS(StringToBoolConverter_TEST)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			// TODO: Your test code here
+		}
+
+	};
+}
